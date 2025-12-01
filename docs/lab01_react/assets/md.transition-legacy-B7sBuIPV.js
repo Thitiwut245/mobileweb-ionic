2f75eef0331ace3e72416f7ce9874e8e0514f952
Thitiwut245 <thitiwut.si@kkumail.com>
2025-12-01 14:25:03 +0700

    Simplify React app (no router)

--- docs/lab01_react/assets/md.transition-legacy-B7sBuIPV.js
+++ docs/lab01_react/assets/md.transition-legacy-B7sBuIPV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B4yTeHsc.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BpkS9Jxd.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.k,i=e.j}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
