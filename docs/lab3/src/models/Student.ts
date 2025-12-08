48a65ca309903111710b58f021649b72cbfbdc00
Thitiwut245 <Thitiwut.si@kkumail.com>
2025-12-08 15:54:32 +0700

    add lad3

---
+++ docs/lab3/src/models/Student.ts
@@ -0,0 +1,11 @@
+export interface Student {
+  id: string;
+
+  title_name: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  //ปีการศึกษา+คณะ
+  year: number;
+  major: string;
+}
