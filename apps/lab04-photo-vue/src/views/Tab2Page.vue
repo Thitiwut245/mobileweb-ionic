07b66d241bf205145ddde9ee4fdc94765a59cada
“Thitiwut245” <Thitiwut.si@kkumail.com>
2026-01-05 17:03:45 +0700

    Add Lab 4 Photo Gallery page and link from index

--- apps/lab04-photo-vue/src/views/Tab2Page.vue
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -1,23 +1,70 @@
+<!-- src/views/Tab2Page.vue -->
 <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Photo Gallery</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+      <!-- Student info (required by lab) -->
+      <div class="student-info ion-padding">
+        <p><strong>รหัสนักศึกษา:</strong> 663380213-6</p>
+        <p><strong>ชื่อ:</strong> ธิติวุฒิ ศรีอมรรัตน์</p>
+      </div>
+
+      <!-- Gallery Grid -->
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" v-for="(p, i) in photos" :key="i">
+            <ion-img :src="p.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <!-- Floating Action Button (Camera) -->
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg
+} from '@ionic/vue'
+
+import { camera } from 'ionicons/icons'
+
+import { usePhotoGallery } from '@/composables/usePhotoGallery'
+
+const { addNewToGallery, photos } = usePhotoGallery()
 </script>
+
+<style scoped>
+.student-info {
+  text-align: center;
+  font-size: 1rem;
+}
+</style>
