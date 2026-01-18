426d5dcc5e190bd5a8a52b883f5ebb73d2b9aaf8
“Thitiwut245” <Thitiwut.si@kkumail.com>
2026-01-18 17:50:08 +0700

    lab5 commit

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { camera } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/react';
+
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">Lab 05 - โดย ธิติวุฒิ ศรีอมรรัตน์ รหัส 663380213-6</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">Lab 05 - โดย ธิติวุฒิ ศรีอมรรัตน์ รหัส 663380213-6</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        {/* Photo Grid */}
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        {/* Camera FAB */}
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
