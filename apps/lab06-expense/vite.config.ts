fa6d644b8b37d49e93c554b634f9232347e1a97b
Thitiwut245 <thitiwut.si@kkumail.com>
2026-03-01 13:57:36 +0700

    fix lab6

--- apps/lab06-expense/vite.config.ts
+++ apps/lab06-expense/vite.config.ts
@@ -7,6 +7,11 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: '../../docs/lab6',
+    emptyOutDir: true
+  },
   plugins: [
     vue(),
     legacy()
